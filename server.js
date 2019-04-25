883007f26ec19bfd16900d3527e9eb52eb3cc575
Pankaj Yadav <pankaj.yadav01@nagarro.com>
2019-04-25 14:34:23 +0530

    something

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-//Install express server
-const express = require('express');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-weather-app'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/my-weather-app/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const express = require('express');  
+const app = express();  
+app.use(express.static(__dirname + '/dist/my-weather-app'));  
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/dist/my-weather-app/index.html');  
+});  
+app.listen(process.env.PORT || 8080);  
